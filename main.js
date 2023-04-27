e0f379977f2cd66b1b67f8cf9e7d5355ef0549d9
robin-sevelin <robinsevelin@hotmail.com>
2023-04-27 22:11:00 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Welcome to the randomizer!</h1>
+    <div class="card">
+    <button id="search-button">Generate</button>
+    </div>
+    <p class="read-the-docs">
+      Click on the button to generate a random person
+    </p>
+    <div id="content"></div>
+  </div>
+`;
+
+const content = document.getElementById('content');
+const searchButton = document.getElementById('search-button');
+
+searchButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  fetchApi();
+});
+
+export async function fetchApi() {
+  await fetch('https://randomuser.me/api/')
+    .then((res) => res.json())
+    .then((data) => {
+      renderCharacter(data);
+    });
+}
+
+function renderCharacter(data) {
+  content.innerHTML = '';
+  let div = document.createElement('div');
+
+  for (let i = 0; i < data.results.length; i++) {
+    div.innerHTML = `<img src="${data.results[i].picture.large}"><br><p>Name: ${data.results[i].name.first} ${data.results[i].name.last}</p>`;
+  }
+
+  content.appendChild(div);
+}
